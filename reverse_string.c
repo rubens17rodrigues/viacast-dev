0a9edd09baf6613c2d96e3f30cb155919740342e
Rubens Rodrigues <rubens17rodrigues@gmail.com>
2021-08-03 03:44:05 -0300

    Atualiza arquivo reverse_string.c

--- reverse_string.c
+++ reverse_string.c
@@ -2,36 +2,39 @@
 #include <string.h>
 
 void reverseString(char *in, char *out) {
-  int i, j;
-  int N = strlen(in);
-  for (i = 0, j = N - 1; j >= 0; ++i, --j) {
-    out[i] = in[j];
-  }
-  out[N] = '\0';
+    int i;
+    int N = strlen(in);
+    for (i = 0; i < N; ++i) {
+        out[i] = in[N-i-1];
+    }
+    out[N] = '\0';
 }
 
 void reverseStringInPlace(char *in) {
-  int i;
-  int N = strlen(in);
-  for (i = 0; i < N/2; ++i) {
-    char c = in[i];
-    in[i] = in[N - i];
-    in[N - i] = c;
-  }
+    int i;
+    int N = strlen(in);
+    for (i = 0; i < N/2; ++i) {
+        char c = in[i];
+        in[i] = in[N -1 - i];
+        in[N -1 -i] = c;
+    }
+    in[N] = '\0';
 }
 
-int main(int argc, char **argv) {
-  char string_in[50];
-  char stringReversed[50];
+int main(int argc, char *argv[]) {
+    char string_in[50];
+    char stringReversed[50];
+   
+    printf("Enter the string to be reversed:\n>>");
+    if (fgets(string_in, sizeof(string_in), stdin)){
+        string_in[strcspn(string_in, "\n")] = '\0';
+    }
 
-  printf("Enter the string to be reversed:\n>>");
-  gets(string_in);
+    reverseString(string_in, stringReversed);
+    printf("reversed = '%s'\n", stringReversed);
 
-  reverseString(string_in, stringReversed);
-  printf("reversed = '%s'\n", stringReversed);
+    reverseStringInPlace(string_in);
+    printf("reversed in place = '%s'\n", string_in);
 
-  reverseStringInPlace(string_in);
-  printf("reversed in place = '%s'\n", string_in);
-
-  return 0;
+    return 0;
 }
