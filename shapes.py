fc6bda933902ffa45cb1310e147746cc1f833f52
Rubens Rodrigues <rubens17rodrigues@gmail.com>
2021-08-03 03:45:30 -0300

    Atualiza arquivos shapes.py

--- shapes.py
+++ shapes.py
@@ -2,53 +2,64 @@ from math import *
 
 
 class Rectangle:
-  def __init__(this, width, height):
-    this.width = width
-    this.height = height
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-  def perimeter(this):
-    return 2*this.width + 2*this.height
+    def perimeter(self):
+        return 2*self.width + 2*self.height
 
-  @property
-  def area(this):
-    return this.width*this.height
+    @property
+    def width(self):
+        print("Getter foi chamado")
+        return self._width
+
+    @width.setter
+    def width(self, new_width):
+        print("Setter foi chamado.")
+        self._width = new_width
+
+    def area(self):
+        return self.width*self.height
 
 
 class IsoscelesTriangle:
-  def __init__(this, base, height):
-    this.base = base
-    this.height = height
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
 
-  def perimeter(this):
-    side = sqrt(this.height**2 + (this.base/2)**2)
-    return 2*side + base
+    def perimeter(self):
+        side = sqrt(self.height**2 + (self.base/2)**2)
+        return 2*side + base
 
-  @property
-  def area(this):
-    return this.base*this.height/2
+    def area(self):
+        return self.base*self.height/2
 
 
 class Circle:
-  def __init__(this, radius):
-    this.radius = radius
+    def __init__(self, radius):
+        self.radius = radius
 
-  def perimeter(this):
-    return 2*pi*this.radius
+    def perimeter(self):
+        return 2*pi*self.radius
 
-  @property
-  def area(this):
-    return pi*this.radius**2
+    def area(self):
+        return pi*self.radius**2
 
 
 if __name__ == '__main__':
-  width, height = map(int, input('Enter the width and height for the rectangle (<width>,<height>):\n>>').split(','))
-  rectangle = Rectangle(width, height)
-  print(f"Area: {rectangle.area}\nPerimeter: {rectangle.perimeter()}")
+    width, height =  map(int, input('Enter the width and height for' \
+        'the rectangle (<width>,<height>):\n>>').split(','))
+   
+    rectangle = Rectangle(width, height)
+    print(f"Area: {rectangle.area()}\nPerimeter: {rectangle.perimeter()}")
 
-  base, height = map(int, input('Enter the base and height for the isosceles triangle (<base>,<height>):\n>>').split(','))
-  triangle = IsoscelesTriangle(base, height)
-  print(f"Area: {triangle.area}\nPerimeter: {triangle.perimeter()}")
+    base, height = map(int, input('Enter the base and height for ' \
+        'the isosceles triangle (<base>,<height>):\n>>').split(','))
 
-  radius = int(input('Enter the radius for the circle:\n>>'))
-  circle = Circle(radius)
-  print(f"Area: {circle.area}\nPerimeter: {circle.perimeter()}")
+    triangle = IsoscelesTriangle(base, height)
+    print(f"Area: {triangle.area()}\nPerimeter: {triangle.perimeter()}")
+
+    radius = int(input('Enter the radius for the circle:\n>>'))
+    circle = Circle(radius)
+    print(f"Area: {circle.area()}\nPerimeter: {circle.perimeter()}")
